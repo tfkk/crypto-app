e0897be4f99e4b73ed4dcc83e80304647a5e32e0
Filip <47536273+tfkk@users.noreply.github.com>
2024-03-29 20:44:33 +0100

    preparing navbar

--- crypto-app/src/scenes/navbar/index.tsx
+++ crypto-app/src/scenes/navbar/index.tsx
@@ -7,7 +7,30 @@ type Props = {}
 const Navbar = (props: Props) => {
 const flexBetween = "flex items-center justify-between";
 
-  return <div> Navbar</div>
+  return <nav>
+    <div className={`${flexBetween} fixed top-0 z-30 w-full`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+            <div className={`${flexBetween} w-full gap-16`}>
+                {/* LEFT SIDE */}
+                <img alt="logo" src={Logo} className="scale-50"/>
+
+                {/* RIGHT SIDE */}
+                <div className={`${flexBetween} w-full`}>
+                    <div className={`${flexBetween} gap-8 text-sm`}>
+                        <p>Home</p>
+                        <p>About</p>
+                        <p>Live rates</p>
+                        <p>Contact us</p>
+                    </div>
+
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </nav>
 }
 
 export default Navbar;
