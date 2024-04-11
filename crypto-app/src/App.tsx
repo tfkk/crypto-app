6296aacc813e287293f78a51030127777aad4abf
Filip <47536273+tfkk@users.noreply.github.com>
2024-04-11 15:01:57 +0200

    created home page section

--- crypto-app/src/App.tsx
+++ crypto-app/src/App.tsx
@@ -1,4 +1,6 @@
 import Navbar from "@/scenes/navbar"
+import Home from "@/scenes/home"
+import About from "@/scenes/about"
 import { useEffect, useState } from "react"
 import { SelectedPage } from "./shared/types";
 
@@ -27,6 +29,8 @@ function App() {
       selectedPage={selectedPage}
       setSelectedPage={setSelectedPage}
     />
+    <Home setSelectedPage={setSelectedPage}/>
+    <About setSelectedPage={setSelectedPage}/>
   </div>
 }
 
